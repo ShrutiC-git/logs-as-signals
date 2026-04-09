5fa80023f2cc3a2c0fc411c741ba346ce529139a
Shruti Chaturvedi <66940685+ShrutiC-git@users.noreply.github.com>
2026-04-09 18:21:21 +0530

    modify test-index to return a log indexed in OpenSearch

--- test-index.sh
+++ test-index.sh
@@ -1,11 +1,15 @@
-curl -X POST "localhost:9200/logs-demo/_doc" \
--H "Content-Type: application/json" \
--d '{
-  "service": "checkout",
-  "endpoint": "/checkout",
-  "latency_ms": 120,
-  "error": "",
-  "retry_count": 0,
-  "status": "success",
-  "@timestamp": "2026-03-19T10:00:00Z"
-}'
+curl -X GET "http://localhost:9200/logs-demo/_search?pretty=true" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "size": 1,
+    "sort": [
+      {
+        "@timestamp": {
+          "order": "desc"
+        }
+      }
+    ],
+    "query": {
+      "match_all": {}
+    }
+  }'
